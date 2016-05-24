c2759c9122bcddbde789ab1d039529844dff6f47
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-24 13:37:40 +0700

    initComponent in UI Log in (temp)

--- LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/MainActivity.java
+++ LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/MainActivity.java
@@ -2,12 +2,36 @@ package techkids.mad3.learninternalexternalstorage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editTextEmail, editTextPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponent();
+    }
+
+    private void initComponent() {
+        editTextEmail = (EditText) this.findViewById(R.id.editTextMail);
+        editTextPassword = (EditText) this.findViewById(R.id.editTextPassword);
+        btnLogin = (Button) this.findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.btn_login) {
+            Log.d("Test", "okkkkkkkkkkkkkkkkkk");
+        }
+
     }
 }
