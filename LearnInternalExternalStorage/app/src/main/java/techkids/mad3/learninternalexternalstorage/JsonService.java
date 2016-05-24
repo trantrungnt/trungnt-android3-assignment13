d06eec5b5da51dd7c1686710dc2d8e51ec880347
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-24 17:16:08 +0700

    login (temp)
    + User: android%40hungdepzai.techkids.vn
    + Password: 123456
    + Put user and password to JsonService

--- LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/JsonService.java
+++ LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/JsonService.java
@@ -2,7 +2,10 @@ package techkids.mad3.learninternalexternalstorage;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,6 +17,9 @@ import java.net.URL;
  * Created by TrungNT on 5/24/2016.
  */
 public class JsonService extends IntentService {
+    private String login_status, login_message, link;
+    private Bundle bundleGetData;
+    private String email, password;
 
     public JsonService() {
         super("JsonService");
@@ -22,9 +28,12 @@ public class JsonService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         URL url = null;
+        bundleGetData = intent.getExtras();
+        email = bundleGetData.getString("Email");
+        password = bundleGetData.getString("Password");
 
         try{
-            url = new URL("http://g-service.herokuapp.com/api/techkids/login?username=android%40hungdepzai.techkids.vn&password=123456");
+            url = new URL("http://g-service.herokuapp.com/api/techkids/login?username="+ email + "&password=" + password);
             InputStreamReader reader = new InputStreamReader(url.openStream(),"UTF-8");
             char[] buff = new char[64];
             StringBuilder x = new StringBuilder();
@@ -34,6 +43,22 @@ public class JsonService extends IntentService {
             }
 
             String result = x.toString();
+            JSONObject resultObject = new JSONObject(result);
+            login_status = resultObject.getString("login_status");
+            login_message = resultObject.getString("login_message");
+
+            switch (login_status)
+            {
+                case "0":
+                        //Mo thong bao cho nguoi dung khong dang nhap duoc
+                        //intent = new
+                    break;
+                case "1":
+                    break;
+            }
+
+//            String link = resultObject.getString("link");
+
             Log.d("Json cua Hung day ak? ", result);
 
         } catch (Exception e) {
--- LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/MainActivity.java
+++ LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/MainActivity.java
@@ -1,5 +1,7 @@
 package techkids.mad3.learninternalexternalstorage;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +13,15 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editTextEmail, editTextPassword;
     private Button btnLogin;
+    private Bundle getBundleData, bundleData;
+    private Intent getIntentData, intentData;
+    private String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initComponent();
-        openJsonService();
     }
 
     private void initComponent() {
@@ -27,19 +31,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnLogin.setOnClickListener(this);
     }
 
-    private void openJsonService()
-    {
-        Intent intent = new Intent(this, JsonService.class);
-        startService(intent);
-    }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
+        email = editTextEmail.getText().toString();
+        password = editTextPassword.getText().toString();
 
         if (id == R.id.btn_login) {
-            Log.d("Test", "okkkkkkkkkkkkkkkkkk");
+            intentData = new Intent(MainActivity.this, JsonService.class);
+            bundleData = new Bundle();
+            bundleData.putString("Email", email);
+            bundleData.putString("Password", password);
+            intentData.putExtras(bundleData);
+            startService(intentData);
         }
 
+    }
+
+    private void showAlertDialog (String titleDialog, String contentDialog)
+    {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
+        builder.setTitle(titleDialog);
+        builder.setMessage(contentDialog);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
     }
 }
