3f12bee2392e441a667c6abecdbb21de55b45eff
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-26 18:59:25 +0700

    UI Main Activity (temp)

---
+++ LearnInternalExternalStorage/app/src/main/java/techkids/mad3/learninternalexternalstorage/MainActivity.java
@@ -0,0 +1,21 @@
+package techkids.mad3.learninternalexternalstorage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by TrungNT on 5/26/2016.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
